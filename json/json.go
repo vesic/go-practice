36949869daf023b8e31f5f2a11d7bd7ee2ff796e
vesic <dusan.vesic@gmail.com>
2023-02-11 09:39:30 +0100

    Create custom JSON encoding

--- json/json.go
+++ json/json.go
@@ -3,6 +3,7 @@ package json
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"strings"
 )
 
@@ -34,4 +35,37 @@ func PromoteField() {
 	encoder.Encode(&user)
 
 	fmt.Println(builder.String())
+}
+
+//
+
+type User struct {
+	Name     string
+	Username string
+}
+
+func (u *User) MarshalJSON() ([]byte, error) {
+	m := map[string]interface{}{
+		"id":        rand.Int63(),
+		"user_id":   u.Name,
+		"user_name": u.Username,
+	}
+
+	jsn, err := json.Marshal(m)
+	return jsn, err
+}
+
+// CustomEncoding
+// Implement the Marshaler interface to create custom JSON encoding.
+func CustomEncoding() {
+	user := User{
+		Name:     "Ervin",
+		Username: "ervin",
+	}
+
+	var builder strings.Builder
+	encoder := json.NewEncoder(&builder)
+	encoder.Encode(&user)
+	fmt.Println(builder.String())
+	// {"id":914288788548139488,"user_id":"Ervin","user_name":"ervin"}
 }
